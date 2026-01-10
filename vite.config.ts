a4f95abd1e63d49f092a8cfe535399a7f1740af6
JarrodKane <kajarrod@gmail.com>
2026-01-10 16:51:55 +1100

    WIP fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [tailwindcss(), react(),],
-  base: process.env.NODE_ENV === 'production' ? '/DeadFunnyClub' : '/',
+  base: '/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
